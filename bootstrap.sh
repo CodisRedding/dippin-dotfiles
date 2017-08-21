a1abed855b8321bde345037e01a2cfa522e4916c
Rocky Assad <rocky@fourq.io>
2017-08-21 17:08:46 -0500

    prep for bitly

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/CodisRedding/dotfiles-1 ./dotfile
+
+cd ./dotfiles
+
+sh init.sh
---
+++ init.sh
@@ -0,0 +1,19 @@
+DOTFILES_ROOT=$(pwd -P)
+
+git pull origin master
+git submodule update --init --recursive
+
+for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '.*' -not -path '*.git*')
+do
+	ln -s $src $HOME
+done
+
+ln -s $DOTFILES_ROOT/bin $HOME
+ln -s $DOTFILES_ROOT/fonts $HOME
+ln -s $DOTFILES_ROOT/init $HOME
+ln -s $DOTFILES_ROOT/.gitconfig $HOME
+
+sh brew.sh
+sh .macos
+
+source ~/.bash_profile
