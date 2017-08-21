1fc0a836624c92ec8119f7f4c7237aa54ffa070d
Rocky Assad <rocky@fourq.io>
2017-08-21 17:17:03 -0500

    fix path

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/CodisRedding/dotfiles-1 ./dotfiles
+git clone https://github.com/CodisRedding/dotfiles-1 dotfiles
 
-cd ./dotfiles
+cd dotfiles
 
 sh init.sh
